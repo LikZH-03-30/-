bd0a6324c2d29f41fdeac915aa34545ada4e03f8
HZchenguanxi <953659450@qq.com>
2022-06-26 21:25:07 +0800

    完成了购物车案例

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,18 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off',
+    'vue/no-mutating-props': 'off',
+    'eol-last': 'off',
+    'vue/no-duplicate-attributes': 'off',
+    'vue/no-parsing-error': 'off'
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <!-- $time_evening  你怎么还没下班呀！ -->
+    <MyHeader title="全场只要一块多" color="green"></MyHeader>
+    <MyGoods v-for="item in goodsList" :key="item.goods_id" :goods='item'></MyGoods>
+    <MyFooter :goodsList='goodsList'></MyFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyHeader from './components/MyHeader.vue'
+import MyGoods from './components/MyGoods.vue'
+import MyFooter from './components/MyFooter.vue'
+import axios from 'axios'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    MyHeader,
+    MyGoods,
+    MyFooter
+  },
+  data() {
+    return {
+      goodsList: []
+    }
+  },
+  created() {
+    this.getGoods()
+  },
+  methods: {
+    async getGoods() {
+      const res = await axios({ url: '/api/cart' })
+      this.goodsList = res.data.list
+      console.log(res)
+    }
   }
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.app {
+  box-sizing: border-box;
+  padding: 50px 0;
+  max-height: 100vh;
+  overflow: auto;
 }
 </style>
